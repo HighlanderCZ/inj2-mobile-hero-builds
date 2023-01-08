85391d372f0996c6b1f5f04162c0392c051cecb8
Jakub Kordovský <jakub@kordovsky.cz>
2023-01-08 10:14:33 +0100

    Example bot command

--- examples/bot-command.js
+++ examples/bot-command.js
@@ -1,3 +1,9 @@
 import { MessageEmbed } from 'discord.js';
 
-export const botCommand = (abbreviation) => {};
+export const botCommand = async (abbreviation) => {
+   const response = await fetch(
+      'https://raw.githubusercontent.com/HighlanderCZ/inj2-mobile-hero-builds/main/builds.json'
+   );
+
+   const builds = await response.json();
+};
