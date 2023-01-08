e18e0aca8a1658aa6af2f55daf4fb5cf0197da66
Jakub Kordovský <jakub@kordovsky.cz>
2023-01-08 19:19:06 +0100

    Added second build for AAAM

--- examples/builds.js
+++ examples/builds.js
@@ -20,8 +20,11 @@ export const getBuild = async (abbreviation) => {
          silver: 0xd0e0e3,
          gold: 0xffd34d,
          legendary: 0xbf48ac,
+         undetermined: 0x98c379,
       };
-      const heroColor = colors[hero.rank.toLowerCase()];
+      const heroColor = colors.hasOwnProperty(hero.rank.toLowerCase())
+         ? colors[hero.rank.toLowerCase()]
+         : colors.undetermined;
 
       heroEmbed.setTitle(hero.name).setDescription(hero.description);
       heroEmbed.setColor(heroColor);
