63364a9197e42feb7ba121d68868414998f8f381
Jakub Kordovský <jakub@kordovsky.cz>
2023-01-08 15:54:54 +0100

    Expanded build info

--- examples/builds.js
+++ examples/builds.js
@@ -1,10 +1,10 @@
 import { MessageEmbed } from 'discord.js';
+import fetch from 'node-fetch';
 
 export const getBuild = async (abbreviation) => {
-   const response = await fetch(
-      //"https://raw.githubusercontent.com/HighlanderCZ/inj2-mobile-hero-builds/main/builds.json"
-      'https://inj2.striky.cz/builds.json'
-   );
+   const endpoint =
+      'https://raw.githubusercontent.com/HighlanderCZ/inj2-mobile-hero-builds/374cbd3fef258bdaf4e11debf7362411918f3242/builds.json';
+   const response = await fetch(endpoint);
 
    if (!response.ok) {
       throw new Error(`An error has occured while fetching data: ${response.status}`);
@@ -26,20 +26,30 @@ export const getBuild = async (abbreviation) => {
       heroEmbed.setTitle(hero.name).setDescription(hero.description);
       heroEmbed.setColor(heroColor);
 
-      heroEmbed.addFields({
-         name: 'Rank',
-         value: hero.rank,
-         inline: true,
-         name: 'Class',
-         value: hero.class,
-         inline: true,
-      });
+      if (hero.footnote && hero.footnote.length > 0) {
+         heroEmbed.setFooter({ text: hero.footnote });
+      }
+
+      heroEmbed.addFields(
+         {
+            name: 'Rank',
+            value: hero.rank,
+            inline: true,
+         },
+         {
+            name: 'Class',
+            value: hero.class,
+            inline: true,
+         }
+      );
 
       if (hero.image) {
          heroEmbed.setImage(hero.image);
       }
 
       if (hero.passives.length > 0) {
+         heroEmbed.addField('\u200B', '\u200B');
+
          hero.passives.forEach((passive) => {
             heroEmbed.addField(passive.name, passive.description);
          });
@@ -54,9 +64,19 @@ export const getBuild = async (abbreviation) => {
          buildEmbed.setColor(heroColor);
 
          if (build.author) {
-            buildEmbed.setAuthor({
+            const author = {
                name: build.author,
-            });
+            };
+
+            if (build.author_icon && build.author_icon.length > 0) {
+               author.iconURL = build.author_icon;
+            }
+
+            if (build.author_url && build.author_url.length > 0) {
+               author.url = build.author_url;
+            }
+
+            buildEmbed.setAuthor(author);
          }
 
          buildEmbed.addField('Gear', build.gear);
